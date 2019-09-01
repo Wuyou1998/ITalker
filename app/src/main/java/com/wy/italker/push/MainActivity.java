68e23ca1fed944bdb3acedea23dbce797d10f0c3
wuyou1998 <1020131041@qq.com>
2019-09-01 13:17:49 +0800

    初次提交

---
+++ app/src/main/java/com/wy/italker/push/MainActivity.java
@@ -0,0 +1,16 @@
+package com.wy.italker.push;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wy.italker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
